a0dc3321402f5068c23624d1c10fe90db093ea9a
aykhumajhb024 <aykhumajhb024@student.wethinkcode.co.za>
2025-01-09 21:54:49 +0200

    added a while loop noe game has more than one turn

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,29 +1,33 @@
 import random
-def game_play():
-    moves = ["rock","paper","scissors"]
-    
 
-    player = input("Enter rock, paper, scissors: ")
-    computer = random.choice(moves)
+def game_play():    
+    while True:
+        player = input("Enter rock, paper, scissors: ")
+        moves = ["rock","paper","scissors"]
+        computer = random.choice(moves)
 
-    if player == computer:
-        print("It's a tie")
-    elif player == "paper":
-        if computer == "rock":
-            print("paper covers rock, Paper wins!")
-        else:
-            print("Scissors cuts paper")
-    elif player == "scissors":
-        if computer == "paper":
-            print("Scissors cuts paper")
-        else:
-            print("rock crushes scissors")
-    elif player == "rock":
-        if computer == "scissors":
-            print("rock crushes scissors")
-        else:
-            print("paper covers rock")
+        if player == computer:
+            print("It's a tie")
+        elif player == "paper":
+            if computer == "rock":
+                print("paper covers rock, Paper wins!")
+            else:
+                print("Scissors cuts paper")
+        elif player == "scissors":
+            if computer == "paper":
+                print("Scissors cuts paper")
+            else:
+                print("rock crushes scissors")
+        elif player == "rock":
+            if computer == "scissors":
+                print("rock crushes scissors")
+            else:
+                print("paper covers rock")
+
+        play_again = input("Do you wnat to play again? (yes/no): ")
+        if play_again.lower() != "yes":
+            break
 
 game_play()
-    
+        
 

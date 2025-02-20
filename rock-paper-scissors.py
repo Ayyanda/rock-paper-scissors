3e63b27ece06b41fc338ed14e3239bb32738e8a3
aykhumajhb024 <aykhumajhb024@student.wethinkcode.co.za>
2025-02-20 21:20:24 +0200

    made the game code more effecient

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,39 +1,24 @@
 import random
 
-def game_play():    
+def game():
     while True:
-        player = input("Enter rock, paper, scissors: ")
-        moves = ["rock","paper","scissors"]
-        computer = random.choice(moves)
+        moves = {'rock':['paper','spock'],'paper':['scissors','lizard'],'scissors':['rock','spock'],'lizard':['rock','scissors'],'spock':['lizard','paper']}
+        list_of_moves = ['rock','paper','scissors','lizard','spock']
 
-        #Checking for invalid moves by player
-        if player not in moves:
-            print("invalid move played")
-            continue
+        user_moves = input("Play: ")
+        computer_moves = random.choice(list_of_moves)
+        print(computer_moves) #to see which move the computer played
 
-        #Determining the winner and loser
-        if player == computer:
-            print("It's a tie")
-        elif player == "paper":
-            if computer == "rock":
-                print("paper covers rock, Paper wins!")
-            else:
-                print("Scissors cuts paper")
-        elif player == "scissors":
-            if computer == "paper":
-                print("Scissors cuts paper, Paper loses!")
-            else:
-                print("rock crushes scissors, Scissors loses!")
-        elif player == "rock":
-            if computer == "scissors":
-                print("rock crushes scissors, Scissors lose!")
-            else:
-                print("paper covers rock, Rock loses!")
+        if computer_moves == user_moves:
+            return 'Draw!!'
 
-        play_again = input("Do you wnat to play again? (yes/no): ")
+        if computer_moves in moves[user_moves]:
+            print('Computer wins')
+        else:
+            print('User wins!!')
+
+        play_again = input("Do you wnat to play again?(yes/no): ")
         if play_again.lower() != "yes":
-            break
+            return "End of Game"
 
-game_play()
-        
-
+print(game())

e54589f8a22465a06cf46b6b49d76ab93e17a990
aykhumajhb024 <aykhumajhb024@student.wethinkcode.co.za>
2025-02-20 21:24:24 +0200

    two computers against each other

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -5,17 +5,17 @@ def game():
         moves = {'rock':['paper','spock'],'paper':['scissors','lizard'],'scissors':['rock','spock'],'lizard':['rock','scissors'],'spock':['lizard','paper']}
         list_of_moves = ['rock','paper','scissors','lizard','spock']
 
-        user_moves = input("Play: ")
-        computer_moves = random.choice(list_of_moves)
-        print(computer_moves) #to see which move the computer played
+        computer_one_moves = random.choice(list_of_moves)
+        computer_two_moves = random.choice(list_of_moves)
+        #print(computer_moves) #to see which move the computer played
 
-        if computer_moves == user_moves:
+        if computer_one_moves == computer_two_moves:
             return 'Draw!!'
 
-        if computer_moves in moves[user_moves]:
-            print('Computer wins')
+        if computer_one_moves in moves[computer_two_moves]:
+            print('Computer one wins')
         else:
-            print('User wins!!')
+            print('Computer two wins!!')
 
         play_again = input("Do you wnat to play again?(yes/no): ")
         if play_again.lower() != "yes":
